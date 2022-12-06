fc5b757432f03e70896ac9e586af04537bdf4755
Sitamsh Rijal <sitamshrijal@gmail.com>
2022-12-06 14:18:54 +0545

    Refactor to be less imperative

--- src/Day6.kt
+++ src/Day6.kt
@@ -5,36 +5,12 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         val data = parse(input)
-
-        data.forEachIndexed { index, _ ->
-            if (index > 3) {
-                val previousValues =
-                    setOf(data[index - 1], data[index - 2], data[index - 3], data[index - 4])
-
-                if (previousValues.size == 4) {
-                    return index
-                }
-            }
-        }
-        return 0
+        return data.windowed(4).indexOfFirst { it.toSet().size == 4 } + 4
     }
 
     fun part2(input: List<String>): Int {
         val data = parse(input)
-
-        data.forEachIndexed { index, _ ->
-            if (index > 13) {
-                val previousValues = buildSet {
-                    (index - 1 downTo index - 14).forEach {
-                        add(data[it])
-                    }
-                }
-                if (previousValues.size == 14) {
-                    return index
-                }
-            }
-        }
-        return 0
+        return data.windowed(14).indexOfFirst { it.toSet().size == 14 } + 14
     }
 
     val input = readInput("input6")
