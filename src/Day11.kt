5462b2425bcd162722809fe9b4e844cac4b8375d
Sitamsh Rijal <sitamshrijal@gmail.com>
2022-12-14 11:42:18 +0545

    Refactor Monkey parsing

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,72 +1,5 @@
 fun main() {
-    fun parse(input: List<String>): List<Monkey> {
-        var monkeyNumber = 0
-        val map = mutableMapOf<Int, Monkey>()
-
-        val monkeyRegex = """Monkey (\d):""".toRegex()
-        val operationRegex = """Operation: new = old (.) (\d+)""".toRegex()
-        val testRegex = """Test: divisible by (\d+)""".toRegex()
-        val ifTrueRegex = """If true: throw to monkey (\d)""".toRegex()
-        val ifFalseRegex = """If false: throw to monkey (\d)""".toRegex()
-
-        val items = mutableListOf<Item>()
-        var operation: (Item) -> Item = { it }
-        var test = Test(0) { false }
-        var ifTrue = 0
-        var ifFalse = 0
-
-        input.forEach { line ->
-            val cleanLine = line.trim()
-            // Monkey
-            if (line matches monkeyRegex) {
-                val (n) = monkeyRegex.matchEntire(line)!!.destructured
-                monkeyNumber = n.toInt()
-            } else {
-                // Starting items
-                if (cleanLine.startsWith("Starting")) {
-                    items.clear()
-                    val worryLevels = cleanLine.substringAfter("Starting items: ")
-                    worryLevels.split(", ").forEach {
-                        items += Item(it.toLong())
-                    }
-                }
-                // Operation
-                if (cleanLine.trim() matches operationRegex) {
-                    val (s, d) = operationRegex.matchEntire(cleanLine)!!.destructured
-
-                    if (s == "*") {
-                        operation = { Item(it.worryLevel * d.toInt()) }
-                    }
-                    if (s == "+") {
-                        operation = { Item(it.worryLevel + d.toInt()) }
-                    }
-                }
-                // Alt operation
-                if (cleanLine == "Operation: new = old * old") {
-                    operation = { Item(it.worryLevel * it.worryLevel) }
-                }
-                // Test
-                if (cleanLine matches testRegex) {
-                    val (d) = testRegex.matchEntire(cleanLine)!!.destructured
-                    test = Test(d.toLong()) { it.worryLevel % d.toLong() == 0L }
-                }
-                // If true
-                if (cleanLine matches ifTrueRegex) {
-                    val (d) = ifTrueRegex.matchEntire(cleanLine)!!.destructured
-                    ifTrue = d.toInt()
-                }
-                // If false
-                if (cleanLine matches ifFalseRegex) {
-                    val (d) = ifFalseRegex.matchEntire(cleanLine)!!.destructured
-                    ifFalse = d.toInt()
-                    val toAdd: List<Item> = items
-                    map[monkeyNumber] =
-                        Monkey(toAdd.toMutableList(), operation, test, ifTrue, ifFalse)
-                }
-            }
-        }
-        return map.values.toList()
-    }
+    fun parse(input: List<String>): List<Monkey> = input.chunked(7).map { Monkey.parse(it) }
 
     fun part1(input: List<String>): Long {
         val monkeys = parse(input)
@@ -89,7 +22,7 @@ fun main() {
     fun part2(input: List<String>): Long {
         val monkeys = parse(input)
 
-        val value = monkeys.map { it.test.divisibleBy }.reduce(Long::times)
+        val value = monkeys.map { it.test }.reduce(Long::times)
         val worryDecrease: (Item) -> (Item) = { Item(it.worryLevel % value) }
 
         repeat(10_000) {
@@ -114,12 +47,10 @@ fun main() {
 @JvmInline
 value class Item(val worryLevel: Long)
 
-data class Test(val divisibleBy: Long, val testFunction: (Item) -> Boolean)
-
 data class Monkey(
     val items: MutableList<Item>,
     val operation: (Item) -> Item,
-    val test: Test,
+    val test: Long,
     val ifTrue: Int,
     val ifFalse: Int,
     var inspectionCount: Long = 0L
@@ -128,11 +59,52 @@ data class Monkey(
         inspectionCount++
         val itemAfterOperation = operation(item)
         val itemAfterWorryDecrease = worryDecrease(itemAfterOperation)
-        val isDivisible = test.testFunction(itemAfterWorryDecrease)
+        val isDivisible = itemAfterWorryDecrease.worryLevel % test == 0L
         return itemAfterWorryDecrease to if (isDivisible) ifTrue else ifFalse
     }
 
     fun receiveItem(item: Item) {
         items += item
+    }
+
+    companion object {
+        fun parse(input: List<String>): Monkey {
+            val items = mutableListOf<Item>()
+
+            val worryLevels = input[1].substringAfter("  Starting items: ")
+            worryLevels.split(", ").forEach {
+                items += Item(it.toLong())
+            }
+
+            val operationText = input[2].substringAfter("Operation: new = old ")
+            val (sign, value) = operationText.split(" ")
+            val isOld = value == "old"
+            val operation: (Item) -> Item = when (sign) {
+                "*" -> {
+                    if (isOld) {
+                        { Item(it.worryLevel * it.worryLevel) }
+                    } else {
+                        { Item(it.worryLevel * value.toLong()) }
+                    }
+                }
+
+                "+" -> {
+                    { Item(it.worryLevel + value.toLong()) }
+                }
+
+                else -> error("Not supported")
+            }
+
+            val isDivisibleBy = input[3].substringAfter("  Test: divisible by ")
+            val test = isDivisibleBy.toLong()
+
+            val ifTrueText = input[4].substringAfter("    If true: throw to monkey ")
+            val ifTrue = ifTrueText.toInt()
+
+            val ifFalseText = input[5].substringAfter("    If false: throw to monkey ")
+            val ifFalse = ifFalseText.toInt()
+
+            return Monkey(items, operation, test, ifTrue, ifFalse)
+        }
     }
 }
