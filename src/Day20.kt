053b8cd4130ec9b4328b0ad73dca6a3467d37482
Sitamsh Rijal <sitamshrijal@gmail.com>
2022-12-27 11:02:35 +0545

    Add solution for Day 20 Part 2

--- src/Day20.kt
+++ src/Day20.kt
@@ -1,8 +1,8 @@
 fun main() {
-    fun parse(input: List<String>, decryptionKey: Int = 1): MutableList<Number> =
-        input.mapIndexed { index, s -> Number(s.toInt() * decryptionKey, index) }.toMutableList()
+    fun parse(input: List<String>, decryptionKey: Long = 1L): MutableList<Number> =
+        input.mapIndexed { index, s -> Number(s.toLong() * decryptionKey, index) }.toMutableList()
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
         val numbers = parse(input)
 
         numbers.indices.forEach { originalIndex ->
@@ -11,12 +11,23 @@ fun main() {
 
             numbers.addWrapped(index + toMove.value, toMove)
         }
-        val index = numbers.indexOfFirst { it.value == 0 }
+        val index = numbers.indexOfFirst { it.value == 0L }
         return listOf(1000, 2000, 3000).sumOf { numbers.getWrapped(index + it).value }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val numbers = parse(input, 811_589_153L)
+
+        repeat(10) {
+            numbers.indices.forEach { originalIndex ->
+                val index = numbers.indexOfFirst { it.index == originalIndex }
+                val toMove = numbers.removeAt(index)
+
+                numbers.addWrapped(index + toMove.value, toMove)
+            }
+        }
+        val index = numbers.indexOfFirst { it.value == 0L }
+        return listOf(1000, 2000, 3000).sumOf { numbers.getWrapped(index + it).value }
     }
 
     val input = readInput("input20")
@@ -24,10 +35,10 @@ fun main() {
     println(part2(input))
 }
 
-data class Number(val value: Int, val index: Int)
+data class Number(val value: Long, val index: Int)
 
 fun <T> MutableList<T>.getWrapped(index: Int): T = this[index % size]
 
-fun <T> MutableList<T>.addWrapped(index: Int, value: T) {
+fun <T> MutableList<T>.addWrapped(index: Long, value: T) {
     add(index.mod(size), value)
 }
