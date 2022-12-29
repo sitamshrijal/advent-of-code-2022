8ed1838c8ce3fbec2e26cf14d7b1789787c385d9
Sitamsh Rijal <sitamshrijal@gmail.com>
2022-12-29 21:32:30 +0545

    Add solution for Day 13

---
+++ src/Day13.kt
@@ -0,0 +1,75 @@
+fun main() {
+    fun parse(input: List<String>): List<Packet> =
+        input.filter { it.isNotEmpty() }.map { Packet.parse(it) }
+
+    fun part1(input: List<String>): Int {
+        val packets = parse(input)
+        var sum = 0
+        packets.chunked(2).forEachIndexed { index, pair ->
+            if (pair[0] < pair[1]) {
+                sum += index + 1
+            }
+        }
+        return sum
+    }
+
+    fun part2(input: List<String>): Int {
+        val packets = parse(input)
+
+        val divider1 = Packet.parse("[[2]]")
+        val divider2 = Packet.parse("[[6]]")
+
+        val i = packets.count { it < divider1 } + 1
+        val j = packets.count { it < divider2 } + 2
+        return i * j
+    }
+
+    val input = readInput("input13")
+    println(part1(input))
+    println(part2(input))
+}
+
+sealed interface Packet : Comparable<Packet> {
+    data class IntPacket(val value: Int) : Packet {
+        override fun compareTo(other: Packet): Int = when (other) {
+            is IntPacket -> value compareTo other.value
+            is ListPacket -> ListPacket(listOf(this)) compareTo other
+        }
+    }
+
+    data class ListPacket(val packets: List<Packet>) : Packet {
+        override fun compareTo(other: Packet): Int = when (other) {
+            is IntPacket -> compareTo(ListPacket(listOf(other)))
+            is ListPacket -> packets.zip(other.packets).map { it.first compareTo it.second }
+                .firstOrNull { it != 0 } ?: (packets.size compareTo other.packets.size)
+        }
+    }
+
+    companion object {
+        fun parse(input: String): Packet {
+            if (input.all { it.isDigit() }) {
+                return IntPacket(input.toInt())
+            }
+            if (input == "[]") {
+                return ListPacket(emptyList())
+            }
+            val cleanInput = input.removeSurrounding("[", "]")
+            val packets = mutableListOf<Packet>()
+
+            var current = 0
+            var level = 0
+            cleanInput.forEachIndexed { index, c ->
+                when (c) {
+                    '[' -> level++
+                    ']' -> level--
+                    ',' -> if (level == 0) {
+                        packets += parse(cleanInput.substring(current, index))
+                        current = index + 1
+                    }
+                }
+            }
+            packets += parse(cleanInput.substring(current))
+            return ListPacket(packets)
+        }
+    }
+}
